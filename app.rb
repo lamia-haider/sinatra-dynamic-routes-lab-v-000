5f8fc19598482fb0f88db8849bbbeb400ec7611a
Lamia Haider <lamia.haider@gmail.com>
2020-12-11 22:51:35 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,4 +3,17 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
+  get '/reversename/:name' do
+    params[:name].reverse!
+  end
+
+  get '/square/:number' do
+    res = params[:number].to_i * params[:number].to_i
+    res.to_s
+  end
+
+  get '/say/:number/:phrase' do
+    puts params[:phrase] * params[:number].to_i * ' '
+  end
+
 end
